fdf92c5ebcf9fa33a48abc31109cdb84dad8d840
zdlopez <zachary.d.lopez@gmail.com>
2014-10-14 15:11:08 -0700

    Uniq complete.

--- src/underbar.js
+++ src/underbar.js
@@ -100,6 +100,14 @@ var _ = {};
 
   // Produce a duplicate-free version of the array.
   _.uniq = function(array) {
+    var result = [];
+
+    _.each(array, function(item, index){
+      if(_.indexOf(result, item) === -1){
+        result.push(item);
+      } 
+    });
+    return result;
   };
 
 
