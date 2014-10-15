912bfe30f1ce95bd13ffe845bd1b2df2e6b1f4fd
zdlopez <zachary.d.lopez@gmail.com>
2014-10-15 14:24:57 -0700

    Defaults complete.

--- src/underbar.js
+++ src/underbar.js
@@ -250,6 +250,14 @@ var _ = {};
   // Like extend, but doesn't ever overwrite a key that already
   // exists in obj
   _.defaults = function(obj) {
+    _.each(arguments, function(item){
+      for(var k in item){
+        if(!obj.hasOwnProperty(k)){
+          obj[k] = item[k];
+        }
+      }
+    });
+    return obj;
   };
 
 
